491821e8caef96ba294cdb90387eb981a52acae4
STD\s20247058 <20247058-takahashikazuto@hcs.ac.jp>
2026-01-21 11:52:40 +0900

    シェーダーぐラグ白黒エフェクトの実装

--- Assets/Scripts/Shader/FlashPostProcessParameter.cs
+++ Assets/Scripts/Shader/FlashPostProcessParameter.cs
@@ -8,6 +8,8 @@ public class FlashPostProcessParameter : MonoBehaviour
         public static readonly int DistortionCenter = Shader.PropertyToID("_DistortionCenter");
         public static readonly int DistortionStrength = Shader.PropertyToID("_DistortionStrength");
 
+        public static readonly int NoiseStrength = Shader.PropertyToID("_NoiseStrength");
+
         public static readonly int PosterizationThreshold = Shader.PropertyToID("_PosterizationThreshold");
         public static readonly int PosterizationLightColor = Shader.PropertyToID("_PosterizationLightColor");
         public static readonly int PosterizationDarkColor = Shader.PropertyToID("_PosterizationDarkColor");
@@ -17,6 +19,8 @@ public class FlashPostProcessParameter : MonoBehaviour
 
     [SerializeField] private Vector2 _distortionCenter = new Vector2(0.5f, 0.5f);
     [SerializeField] private float _distortionStrength = 0.1f;
+
+    [SerializeField] private float _noiseStrength = 1000f;
 
     [SerializeField] private float _posterizationThreshold = 0.1f;
     [SerializeField, ColorUsage(false, true)] private Color _posterizationLightColor = Color.white;
@@ -28,6 +32,8 @@ public class FlashPostProcessParameter : MonoBehaviour
     {
         Shader.SetGlobalVector(ShaderProperty.DistortionCenter, _distortionCenter);
         Shader.SetGlobalFloat(ShaderProperty.DistortionStrength, _distortionStrength);
+
+        Shader.SetGlobalFloat(ShaderProperty.NoiseStrength, _noiseStrength);
 
         Shader.SetGlobalFloat(ShaderProperty.PosterizationThreshold, _posterizationThreshold);
         Shader.SetGlobalColor(ShaderProperty.PosterizationLightColor, _posterizationLightColor);
